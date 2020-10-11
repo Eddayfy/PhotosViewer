caacc695fa86474206f6deb541e5011513755777
Eddayfy <46305054+Eddayfy@users.noreply.github.com>
2020-10-11 21:07:45 +0100

    Photos Viewer

--- PhotosViewer/PhotosViewer.cs
+++ PhotosViewer/PhotosViewer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
@@ -34,23 +34,43 @@ namespace PhotosViewer
             }
 
             if (ImagePathDirectory.Count != 0)
-                InitializeImageOnPictureBox(ImagePathDirectory[PictureIndex]);
+                InitializeTheImage(ImagePathDirectory[PictureIndex]);
         }
 
-        private void InitializeImageOnPictureBox(string path)
+        private void InitializeTheImage(string path)
         {
             if (IsValideImageFormat(path))
             {
                 if (path.EndsWith(".gif"))
-                    ImagePictureBox.Image = Image.FromFile(path);
+                {
+                    PictureBox.Image = Image.FromFile(path);
+                    PictureBox.Visible = true;
+                    ImagePanel.BackgroundImage = null;
+
+                    PictureBox.SizeMode = PictureBoxSizeMode.CenterImage;
+                }
                 else
                 {
-                    Bitmap BitMap = new Bitmap(Image.FromFile(path));
-                    ImagePictureBox.Image = BitMap;
-                }
+                    PictureBox.Image = null;
+                    PictureBox.Visible = false;
 
-                PictureWidth = ImagePictureBox.Image.Width;
-                PictureHeight = ImagePictureBox.Image.Height;
+                    Bitmap Bitmap = new Bitmap(path);
+
+                    int Width = Bitmap.Width, Height = Bitmap.Height;
+
+                    if (Width > ImagePanel.Width)
+                        Width = ImagePanel.Width;
+
+                    if (Height > ImagePanel.Height)
+                        Height = ImagePanel.Height;
+
+                    Bitmap = new Bitmap(Image.FromFile(path), Width, Height);
+
+                    ImagePanel.BackgroundImage = Bitmap;
+
+                    PictureWidth = Width;
+                    PictureHeight = Height;
+                }
 
                 AdjustNextAndPreviousPanel();
 
@@ -58,13 +78,11 @@ namespace PhotosViewer
             }
         }
 
-        bool IsValideImageFormat(string path)
+        private bool IsValideImageFormat(string path)
         {
             if (Path.GetExtension(path) == ".jpg" || Path.GetExtension(path) == ".jpeg" || Path.GetExtension(path) == ".png"
                 || Path.GetExtension(path) == ".bmp" || Path.GetExtension(path) == ".gif" || Path.GetExtension(path) == ".ico")
-            {
                 return true;
-            }
             else
                 return false;
         }
@@ -72,13 +90,13 @@ namespace PhotosViewer
         private void NextPanel_Click(object sender, EventArgs e)
         {
             if (PictureIndex < ImagePathDirectory.Count - 1)
-                InitializeImageOnPictureBox(ImagePathDirectory[++PictureIndex]);
+                InitializeTheImage(ImagePathDirectory[++PictureIndex]);
         }
 
         private void PreviousPanel_Click(object sender, EventArgs e)
         {
             if (PictureIndex > 0)
-                InitializeImageOnPictureBox(ImagePathDirectory[--PictureIndex]);
+                InitializeTheImage(ImagePathDirectory[--PictureIndex]);
         }
 
         private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
@@ -96,7 +114,7 @@ namespace PhotosViewer
                 ImagePathDirectory.Add(OFD.FileName);
                 PictureIndex = 0;
 
-                InitializeImageOnPictureBox(ImagePathDirectory[PictureIndex]);
+                InitializeTheImage(ImagePathDirectory[PictureIndex]);
             }
         }
 
@@ -130,7 +148,7 @@ namespace PhotosViewer
             }
 
             if (ImagePathDirectory.Count != 0)
-                InitializeImageOnPictureBox(ImagePathDirectory[PictureIndex]);
+                InitializeTheImage(ImagePathDirectory[PictureIndex]);
         }
 
         private void GetOtherPictures(string path)
@@ -157,60 +175,38 @@ namespace PhotosViewer
 
         private void AdjustNextAndPreviousPanel()
         {
-            if (ImagePathDirectory.Count > 1)
+            if (!IsZoomOn)
             {
-                if (PictureIndex == 0)
+                if (ImagePathDirectory.Count > 1)
                 {
-                    NextPanel.Visible = true;
-                    PreviousPanel.Visible = false;
-                }
-                else if (PictureIndex == ImagePathDirectory.Count - 1)
-                {
-                    NextPanel.Visible = false;
-                    PreviousPanel.Visible = true;
+                    if (PictureIndex == 0)
+                    {
+                        NextPanel.Visible = true;
+                        PreviousPanel.Visible = false;
+                    }
+                    else if (PictureIndex == ImagePathDirectory.Count - 1)
+                    {
+                        NextPanel.Visible = false;
+                        PreviousPanel.Visible = true;
+                    }
+                    else
+                    {
+                        NextPanel.Visible = true;
+                        PreviousPanel.Visible = true;
+                    }
                 }
                 else
                 {
-                    NextPanel.Visible = true;
-                    PreviousPanel.Visible = true;
+                    NextPanel.Visible = false;
+                    PreviousPanel.Visible = false;
                 }
             }
-            else
-            {
-                NextPanel.Visible = false;
-                PreviousPanel.Visible = false;
-            }
-
-            PictureScale = 1.0f;
-            IsZoomOn = false;
         }
 
         private void getOtherFilesToolStripMenuItem_Click(object sender, EventArgs e)
         {
             if (ImagePathDirectory.Count != 0)
                 GetOtherPictures(ImagePathDirectory[PictureIndex]);
-        }
-
-        private void AppTimer_Tick(object sender, EventArgs e)
-        {
-            if (!IsZoomOn)
-            {
-                ImagePictureBox.Width = ImagePanel.Width;
-                ImagePictureBox.Height = ImagePanel.Height;
-            }
-        }
-
-        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            DialogResult Result = MessageBox.Show(
-                "You are about to close this App.\nAre you sure you want to Exit?",
-                "Exit and Close this App?", MessageBoxButtons.YesNo,
-                MessageBoxIcon.Information, MessageBoxDefaultButton.Button2);
-
-            if (Result == DialogResult.Yes)
-            {
-                this.Close();
-            }
         }
 
         private void ZoomInToolStripMenuItem_Click(object sender, EventArgs e)
@@ -220,21 +216,19 @@ namespace PhotosViewer
                 IsZoomOn = true;
                 PictureScale += 1.0f;
 
+                ImagePanel.Dock = DockStyle.None;
+                NextPanel.Visible = false;
+                PreviousPanel.Visible = false;
+
                 if (PictureScale <= 6)
                 {
-                    Bitmap BitMap = new Bitmap(ImagePictureBox.Image, (int)(PictureScale * PictureWidth), (int)(PictureScale * PictureHeight));
-                    ImagePictureBox.Width = (int)(PictureScale * PictureWidth);
-                    ImagePictureBox.Height = (int)(PictureScale * PictureHeight);
-                    ImagePictureBox.Image = BitMap;
+                    Bitmap BitMap = new Bitmap(ImagePanel.BackgroundImage, (int)(PictureScale * PictureWidth), (int)(PictureScale * PictureHeight));
+                    ImagePanel.Width = (int)(PictureScale * PictureWidth);
+                    ImagePanel.Height = (int)(PictureScale * PictureHeight);
+                    ImagePanel.BackgroundImage = BitMap;
                 }
                 else
                     PictureScale = 6.0f;
-            }
-            else
-            {
-                ImagePictureBox.Image = Image.FromFile(ImagePathDirectory[PictureIndex]);
-                PictureWidth = ImagePictureBox.Image.Width;
-                PictureHeight = ImagePictureBox.Image.Height;
             }
         }
 
@@ -247,23 +241,52 @@ namespace PhotosViewer
 
                 if (PictureScale > 1)
                 {
-                    Bitmap BitMap = new Bitmap(ImagePictureBox.Image, (int)(PictureScale * PictureWidth), (int)(PictureScale * PictureHeight));
-                    ImagePictureBox.Width = (int)(PictureScale * PictureWidth);
-                    ImagePictureBox.Height = (int)(PictureScale * PictureHeight);
-                    ImagePictureBox.Image = BitMap;
+                    Bitmap Bitmap = new Bitmap(ImagePanel.BackgroundImage, (int)(PictureScale * PictureWidth), (int)(PictureScale * PictureHeight));
+                    ImagePanel.Width = (int)(PictureScale * PictureWidth);
+                    ImagePanel.Height = (int)(PictureScale * PictureHeight);
+                    ImagePanel.BackgroundImage = Bitmap;
                 }
                 else
                 {
-                    PictureScale = 1.0f;
                     IsZoomOn = false;
+                    PictureScale = 1.0f;
+                    ImagePanel.Dock = DockStyle.Fill;
+
+                    InitializeTheImage(ImagePathDirectory[PictureIndex]);
                 }
             }
+        }
+
+        private void CovertBackColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ImagePanel.BackColor == Color.Black)
+                ImagePanel.BackColor = Color.White;
+            else if (ImagePanel.BackColor == Color.White)
+                ImagePanel.BackColor = Color.Black;
+        }
+
+        private void PhotosViewer_Resize(object sender, EventArgs e)
+        {
+            if (!IsZoomOn)
+                InitializeTheImage(ImagePathDirectory[PictureIndex]);
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void PhotosViewer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult Result = MessageBox.Show(
+                "You are about to close this App.\nAre you sure you want to Exit?",
+                "Exit and Close this App?", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information, MessageBoxDefaultButton.Button2);
+
+            if (Result == DialogResult.Yes)
+                e.Cancel = false;
             else
-            {
-                ImagePictureBox.Image = Image.FromFile(ImagePathDirectory[PictureIndex]);
-                PictureWidth = ImagePictureBox.Image.Width;
-                PictureHeight = ImagePictureBox.Image.Height;
-            }
+                e.Cancel = true;
         }
 
         private void NewWindowToolStripMenuItem_Click(object sender, EventArgs e)
